62ebcc5b930ff7214bedc55be40a7f871ae1d7ca
Jonathan McMahon <jonathan.mcmahon@radiantsolutions.com>
2019-11-13 19:30:04 -0500

    k_maxpooling.py tf 2.0 conversion

--- k_maxpooling.py
+++ k_maxpooling.py
@@ -1,29 +1,30 @@
-from keras.engine import Layer, InputSpec
-from keras.layers import Flatten
 import tensorflow as tf
 
 
-class KMaxPooling(Layer):
+class KMaxPooling(tf.keras.layers.Layer):
     """
     K-max pooling layer that extracts the k-highest activations from a sequence (2nd dimension).
     TensorFlow backend.
     """
 
-    def __init__(self, k=1, sorted=True, **kwargs):
+    def __init__(self, k=1, sort=True, **kwargs):
         super().__init__(**kwargs)
-        self.input_spec = InputSpec(ndim=3)
+        self.input_spec = tf.keras.layers.InputSpec(ndim=3)
         self.k = k
-        self.sorted = sorted
+        self.sort = sort
+
+    def get_config(self):
+        super().get_config()
 
     def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.k, input_shape[2])
+        return input_shape[0], self.k, input_shape[2]
 
     def call(self, inputs):
         # swap last two dimensions since top_k will be applied along the last dimension
         shifted_inputs = tf.transpose(inputs, [0, 2, 1])
 
         # extract top_k, returns two tensors [values, indices]
-        top_k = tf.nn.top_k(shifted_inputs, k=self.k, sorted=self.sorted)[0]
+        top_k = tf.math.top_k(shifted_inputs, k=self.k, sorted=self.sort)[0]
 
         # return flattened output
         return tf.transpose(top_k, [0, 2, 1])
