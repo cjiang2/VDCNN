6d71a3cab5d96028758d8c2ca15509fe5dda0dce
zonetrooper32 <zone3692@hotmail.com>
2021-01-02 17:50:31 -0700

    Re-implement VDCNN model codebase. To-dos updated.

--- README.md
+++ README.md
@@ -1,98 +1,30 @@
 # VDCNN
 Tensorflow Implementation of Very Deep Convolutional Neural Network for Text Classification.
 
+## Update
+As it turns out, the original repo has too many implementation error (fresh eyes after many years), and it is extremely outdated (Tensorflow has updated to 2.0+ with eager support). Works are being done slowly on updating this code repo.
+
+(Side Note: PyTorch is so much better. :O )
+
+To-Dos:
+
+ - [x] Re-implement VDCNN, correctly. 
+    - [x] 3 types of pooling operations. (Convolutional pooling, k-max pooling, max pooling)
+    - [x] Dotted line shortcuts. (Conv1x1 projection, identity with zero-padding)
+    - [] Double-check re-implementation.
+
+ - [] Dataset codebase. Need to refer to newer tf.datasets implementation.
+
+ - [] Training and evaluating codebase. This should be simpler with tf.keras, once dataset objects are done.
+
 ## Note
-This repository is a simple Keras implementation of VDCNN model proposed by Conneau et al. [Paper](https://arxiv.org/abs/1606.01781) for VDCNN.
-
-Note: Temporal batch norm not implemented. "Temp batch norm applies same kind of regularization as batch norm, except that the activations in a mini-batch are jointly normalized over temporal instead of spatial locations." Right now this project is using regular Tensorflow batch normalization only.
-
-See another [VDCNN implementation in Pytorch](https://github.com/ArdalanM/nlp-benchmarks) if you feel more comfortable with Pytorch, in which the author is having detailed reproduced results as well. See the original [Tensorflow implementation](https://github.com/zonetrooper32/VDCNN/tree/tensorflow_version) as well.
-
-It should be noted that the VDCNN paper states that the implementation is done originally in Touch 7.
+This repository is a Tensorflow implementation of VDCNN model proposed by Conneau et al. [Paper](https://arxiv.org/abs/1606.01781) for VDCNN.
 
 ## Prerequisites
 
  - Python3
- - Tensorflow 1.0 or higher
- - keras 2.1.5 or higher
+ - Tensorflow >= 2.0
  - Numpy
-
-## Datasets
-The original paper tests several NLP datasets, including DBPedia, AG's News, Sogou News and etc. "data_helper.py" operates with CSV format train and test files.
-
-Downloads of those NLP text classification datasets can be found here (Many thanks to ArdalanM):
-
-| Dataset                | Classes | Train samples | Test samples | source |
-|------------------------|:---------:|:---------------:|:--------------:|:--------:|
-| AG’s News              |    4    |    120 000    |     7 600    |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| Sogou News             |    5    |    450 000    |    60 000    |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| DBPedia                |    14   |    560 000    |    70 000    |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| Yelp Review Polarity   |    2    |    560 000    |    38 000    |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| Yelp Review Full       |    5    |    650 000    |    50 000    |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| Yahoo! Answers         |    10   |   1 400 000   |    60 000    |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| Amazon Review Full     |    5    |   3 000 000   |    650 000   |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-| Amazon Review Polarity |    2    |   3 600 000   |    400 000   |[link](https://drive.google.com/drive/u/0/folders/0Bz8a_Dbh9Qhbfll6bVpmNUtUcFdjYmF2SEpmZUZUcVNiMUw1TWN6RDV3a0JHT3kxLVhVR2M)|
-
-## Parameters Setting
-For all versions of VDCNN, training and testing is done on a Ubuntu 16.04 Server with Tesla K80, with Momentum Optimizer of decay 0.9, exponential learning rate decay, a evaluation interval of 25, a batch size of 128. Weights are initialized by He initialization proposed in [He et al](https://arxiv.org/pdf/1502.01852). Batch normalizations are using a decay of 0.999.
-
-(There are tons of factors that can influence the testing accuracy of the model, but overall this project should be good to go. Training of a deep CNN model is not a easy task, patience is everything. -_-)
-
-## Experiments
-
-TODO: Testing of more NLP benchmark datasets and presenting detailed results.
-
-Results are reported as follows:  (i) / (ii)
- - (i): Test set accuracy reported by the paper (acc = 100% - error_rate)
- - (ii): Test set accuracy reproduced by this Keras implementation
-
-Results for Max Pooling:
-
-|      Depth      |      ag_news      |      DBPedia      |     Sogou News    |
-|:---------------:|:-----------------:|:-----------------:|:-----------------:|
-|     9 layers    |  90.83 / xx.xxxx  |  98.65 / xx.xxxx  |  96.30 / xx.xxxx  |  
-|    17 layers    |  91.12 / xx.xxxx  |  98.60 / xx.xxxx  |  96.46 / xx.xxxx  |
-|    29 layers    |  91.27 / xx.xxxx  |  98.71 / xx.xxxx  |  96.64 / xx.xxxx  |
-
-Results for K-max Pooling:
-
-|      Depth      |      ag_news      |      DBPedia      |     Sogou News    |
-|:---------------:|:-----------------:|:-----------------:|:-----------------:|
-|     9 layers    |  90.17 / xx.xxxx  |  98.44 / xx.xxxx  |  96.42 / xx.xxxx  |  
-|    17 layers    |  90.61 / xx.xxxx  |  98.39 / xx.xxxx  |  96.49 / xx.xxxx  |
-|    29 layers    |  91.33 / xx.xxxx  |  98.59 / xx.xxxx  |  96.82 / xx.xxxx  |
-
-Results for Conv downsampling:
-
-|      Depth      |      ag_news      |      DBPedia      |     Sogou News    |
-|:---------------:|:-----------------:|:-----------------:|:-----------------:|
-|     9 layers    |  90.17 / xx.xxxx  |  98.44 / xx.xxxx  |  96.42 / xx.xxxx  |  
-|    17 layers    |  90.61 / xx.xxxx  |  98.39 / xx.xxxx  |  96.49 / xx.xxxx  |
-|    29 layers    |  91.33 / xx.xxxx  |  98.59 / xx.xxxx  |  96.82 / xx.xxxx  |
-
-Results for Max Pooling with Shortcut:
-
-|      Depth      |      ag_news      |      DBPedia      |     Sogou News    |
-|:---------------:|:-----------------:|:-----------------:|:-----------------:|
-|     9 layers    |  90.83 / xx.xxxx  |  98.65 / xx.xxxx  |  96.30 / xx.xxxx  |  
-|    17 layers    |  91.12 / xx.xxxx  |  98.60 / xx.xxxx  |  96.46 / xx.xxxx  |
-|    29 layers    |  91.27 / xx.xxxx  |  98.71 / xx.xxxx  |  96.64 / xx.xxxx  |
-
-Results for K-max Pooling with Shortcut:
-
-|      Depth      |      ag_news      |      DBPedia      |     Sogou News    |
-|:---------------:|:-----------------:|:-----------------:|:-----------------:|
-|     9 layers    |  90.17 / xx.xxxx  |  98.44 / xx.xxxx  |  96.42 / xx.xxxx  |  
-|    17 layers    |  90.61 / xx.xxxx  |  98.39 / xx.xxxx  |  96.49 / xx.xxxx  |
-|    29 layers    |  91.33 / xx.xxxx  |  98.59 / xx.xxxx  |  96.82 / xx.xxxx  |
-
-Results for Conv downsampling with Shortcut:
-
-|      Depth      |      ag_news      |      DBPedia      |     Sogou News    |
-|:---------------:|:-----------------:|:-----------------:|:-----------------:|
-|     9 layers    |  90.17 / xx.xxxx  |  98.44 / xx.xxxx  |  96.42 / xx.xxxx  |  
-|    17 layers    |  90.61 / xx.xxxx  |  98.39 / xx.xxxx  |  96.49 / xx.xxxx  |
-|    29 layers    |  91.33 / xx.xxxx  |  98.59 / xx.xxxx  |  96.82 / xx.xxxx  |
 
 ## Reference
 [Original preprocessing codes and VDCNN Implementation By geduo15](https://github.com/geduo15/Very-Deep-Convolutional-Networks-for-Natural-Language-Processing-in-tensorflow)
--- vdcnn.py
+++ vdcnn.py
@@ -1,116 +1,287 @@
-import keras
-from keras.models import Model
-from keras.layers import Input, Embedding, Conv1D, BatchNormalization, Activation, Add, MaxPooling1D, Dense, Flatten
-from keras.engine.topology import get_source_inputs
-from k_maxpooling import *
+import tensorflow as tf
+from tensorflow.keras import Model, layers, Sequential
 
-def identity_block(inputs, filters, kernel_size=3, use_bias=False, shortcut=False):
-    conv1 = Conv1D(filters=filters, kernel_size=kernel_size, strides=1, padding='same')(inputs)
-    bn1 = BatchNormalization()(conv1)
-    relu = Activation('relu')(bn1)
-    conv2 = Conv1D(filters=filters, kernel_size=kernel_size, strides=1, padding='same')(relu)
-    out = BatchNormalization()(conv2)
-    if shortcut:
-        out = Add()([out, inputs])
-    return Activation('relu')(out)
+N_BLOCKS = {9: (1, 1, 1, 1),
+            17: (2, 2, 2, 2),
+            29: (5, 5, 2, 2),
+            49:(8, 8, 5, 3)}
 
-def conv_block(inputs, filters, kernel_size=3, use_bias=False, shortcut=False, 
-               pool_type='max', sorted=True, stage=1):
-    conv1 = Conv1D(filters=filters, kernel_size=kernel_size, strides=1, padding='same')(inputs)
-    bn1 = BatchNormalization()(conv1)
-    relu1 = Activation('relu')(bn1)
+class KMaxPooling(layers.Layer):
+    """
+    K-max pooling layer that extracts the k-highest activations from a sequence (2nd dimension).
+    TensorFlow backend.
+    """
+    def __init__(self, 
+                 k=None, 
+                 sorted=False):
+        super(KMaxPooling, self).__init__()
+        self.k = k
+        self.sorted = sorted
 
-    conv2 = Conv1D(filters=filters, kernel_size=kernel_size, strides=1, padding='same')(relu1)
-    out = BatchNormalization()(conv2)
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.k, input_shape[2])
 
-    if shortcut:
-        residual = Conv1D(filters=filters, kernel_size=1, strides=2, name='shortcut_conv1d_%d' % stage)(inputs)
-        residual = BatchNormalization(name='shortcut_batch_normalization_%d' % stage)(residual)
-        out = downsample(out, pool_type=pool_type, sorted=sorted, stage=stage)
-        out = Add()([out, residual])
-        out = Activation('relu')(out)
-    else:
-        out = Activation('relu')(out)
-        out = downsample(out, pool_type=pool_type, sorted=sorted, stage=stage)
-    if pool_type is not None:
-        out = Conv1D(filters=2*filters, kernel_size=1, strides=1, padding='same', name='1_1_conv_%d' % stage)(out)
-        out = BatchNormalization(name='1_1_batch_normalization_%d' % stage)(out)
-    return out
+    def call(self, 
+             inputs):
+        if self.k is None:
+            k = int(inputs.shape[1] / 2)
+        else:
+            k = self.k
 
-def downsample(inputs, pool_type='max', sorted=True, stage=1):
-    if pool_type == 'max':
-        out = MaxPooling1D(pool_size=3, strides=2, padding='same', name='pool_%d' % stage)(inputs)
-    elif pool_type == 'k_max':
-        k = int(inputs._keras_shape[1]/2)
-        out = KMaxPooling(k=k, sorted=sorted, name='pool_%d' % stage)(inputs)
-    elif pool_type == 'conv':
-        out = Conv1D(filters=inputs._keras_shape[-1], kernel_size=3, strides=2, padding='same', name='pool_%d' % stage)(inputs)
-        out = BatchNormalization()(out)
-    elif pool_type is None:
-        out = inputs
-    else:
-        raise ValueError('unsupported pooling type!')
-    return out
+        # Swap last two dimensions since top_k will be applied along the last dimension
+        shifted_inputs = tf.transpose(inputs, [0, 2, 1])
+        
+        # Extract top_k, returns two tensors [values, indices]
+        top_k = tf.nn.top_k(shifted_inputs, k=k, sorted=self.sorted)[0]
+        
+        # return flattened output
+        return tf.transpose(top_k, [0, 2, 1])
 
-def VDCNN(num_classes, depth=9, sequence_length=1024, embedding_dim=16, 
-          shortcut=False, pool_type='max', sorted=True, use_bias=False, input_tensor=None):
-    if depth == 9:
-        num_conv_blocks = (1, 1, 1, 1)
-    elif depth == 17:
-        num_conv_blocks = (2, 2, 2, 2)
-    elif depth == 29:
-        num_conv_blocks = (5, 5, 2, 2)
-    elif depth == 49:
-        num_conv_blocks = (8, 8, 5, 3)
-    else:
-        raise ValueError('unsupported depth for VDCNN.')
+class Pooling(layers.Layer):
+    """Wrapper for different pooling operations.
+    Including maxpooling and k-maxpooling.
+    """
+    def __init__(self, 
+                 pool_type='max',
+                 name=None):
+        super(Pooling, self).__init__(name=name)
+        assert pool_type in ['max', 'k_max']
+        self.pool_type = pool_type
 
-    inputs = Input(shape=(sequence_length, ), name='inputs')
-    embedded_chars = Embedding(input_dim=sequence_length, output_dim=embedding_dim)(inputs)
-    out = Conv1D(filters=64, kernel_size=3, strides=1, padding='same', name='temp_conv')(embedded_chars)
+        if pool_type == 'max':
+            self.pool = layers.MaxPooling1D(pool_size=3, strides=2, padding='same')
+        elif pool_type == 'k_max':
+            self.pool = KMaxPooling()
+        
+    def call(self, 
+             x):
+        return self.pool(x)
 
-    # Convolutional Block 64
-    for _ in range(num_conv_blocks[0] - 1):
-        out = identity_block(out, filters=64, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-    out = conv_block(out, filters=64, kernel_size=3, use_bias=use_bias, shortcut=shortcut, 
-                     pool_type=pool_type, sorted=sorted, stage=1)
+class ZeroPadding(layers.Layer):
+    def __init__(self, 
+                 values,
+                 name=None):
+        super(ZeroPadding, self).__init__(name=name)
+        self.values = values
 
-    # Convolutional Block 128
-    for _ in range(num_conv_blocks[1] - 1):
-        out = identity_block(out, filters=128, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-    out = conv_block(out, filters=128, kernel_size=3, use_bias=use_bias, shortcut=shortcut, 
-                     pool_type=pool_type, sorted=sorted, stage=2)
+    def call(self, 
+             x):
+        x = tf.pad(x, [[0, 0], [0, 0], [self.values[0], self.values[1]]], 
+                   mode='CONSTANT', constant_values=0)
+        return x
 
-    # Convolutional Block 256
-    for _ in range(num_conv_blocks[2] - 1):
-        out = identity_block(out, filters=256, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-    out = conv_block(out, filters=256, kernel_size=3, use_bias=use_bias, shortcut=shortcut, 
-                     pool_type=pool_type, sorted=sorted, stage=3)
+class ConvBlock(layers.Layer):
+    """Conv block with downsampling.
+    1x1 conv to increase dimensions.
+    """
+    def __init__(self, 
+                 filters, 
+                 kernel_size=3,
+                 use_bias=True,
+                 shortcut=True,
+                 pool_type=None,
+                 proj_type=None,
+                 name=None,
+                 ):
+        super(ConvBlock, self).__init__(name=name)
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.use_bias = use_bias
+        self.shortcut = shortcut
+        self.pool_type = pool_type
+        self.proj_type = proj_type
 
-    # Convolutional Block 512
-    for _ in range(num_conv_blocks[3] - 1):
-        out = identity_block(out, filters=512, kernel_size=3, use_bias=use_bias, shortcut=shortcut)
-    out = conv_block(out, filters=512, kernel_size=3, use_bias=use_bias, shortcut=False, 
-                     pool_type=None, stage=4)
+        # Deal with downsample and pooling
+        assert pool_type in ['max', 'k_max', 'conv', None]
+        if pool_type is None:
+            strides = 1
+            self.pool = None
+            self.downsample = None
 
-    # k-max pooling with k = 8
-    out = KMaxPooling(k=8, sorted=True)(out)
-    out = Flatten()(out)
+        elif pool_type == 'conv':
+            strides = 2     # Convolutional pooling with stride 2
+            self.pool = None
+            self.downsample = Sequential([layers.Conv1D(filters, 3, strides=2, padding='same', use_bias=use_bias),
+                                          layers.BatchNormalization()])
+        
+        else:
+            strides = 1
+            self.pool = Pooling(pool_type)
+            self.downsample = Sequential([layers.Conv1D(filters, 3, strides=2, padding='same', use_bias=use_bias),
+                                          layers.BatchNormalization()])
 
-    # Dense Layers
-    out = Dense(2048, activation='relu')(out)
-    out = Dense(2048, activation='relu')(out)
-    out = Dense(num_classes, activation='softmax')(out)
+        self.conv1 = layers.Conv1D(filters, kernel_size, strides=strides, padding='same', use_bias=use_bias)
+        self.bn1 = layers.BatchNormalization()
 
-    if input_tensor is not None:
-        inputs = get_source_inputs(input_tensor)
-    else:
-        inputs = inputs
+        self.conv2 = layers.Conv1D(filters, kernel_size, strides=1, padding='same', use_bias=use_bias)
+        self.bn2 = layers.BatchNormalization()
 
-    # Create model.
-    model = Model(inputs=inputs, outputs=out, name='VDCNN')
-    return model
+        assert proj_type in ['identity', 'conv', None]
+        if proj_type == 'conv':
+            # 1x1 conv for projection
+            self.proj = Sequential([layers.Conv1D(filters*2, 1, strides=1, padding='same', use_bias=use_bias), 
+                                    layers.BatchNormalization()])
+        elif proj_type == 'identity':
+            # Identity using zero padding
+            self.proj = ZeroPadding([int(filters // 2), filters - int(filters // 2)])
+
+    def call(self, 
+             x):
+        residual = x
+        print('x:', x.shape)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = tf.nn.relu(out)
+        print('out:', out.shape)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        print('out:', out.shape)
+
+        if self.pool is not None:
+            out = self.pool(out)
+
+        if self.shortcut:
+            if self.downsample is not None:
+                residual = self.downsample(residual)
+            out += residual
+
+        out = tf.nn.relu(out)
+
+        if self.proj_type is not None:
+            out = self.proj(out)
+
+        return out
+
+class VDCNN(Model):
+    """Model codebase for VDCNN.
+    Args:
+        num_classes: No. classes for classification task.
+        depth: depth of VDCNN, one of [9, 17, 29, 49].
+        seqlen: Sequence length.
+        embed_dim: dim for character embeddings.
+        shortcut: Use skip connections.
+        pool_type: Pooling operations to be used, one of ['max', 'k_max', 'conv'].
+        proj_type: Operation to increase dim for dotted skip connection, one of ['identity', 'conv'].
+        use_bias: Use bias for all layers or not.
+        logits: If False, return softmax probs.
+    """
+    def __init__(self, 
+                 num_classes,
+                 depth=9, 
+                 seqlen=1024,
+                 embed_dim=16,
+                 shortcut=True, 
+                 pool_type='max',
+                 proj_type='conv',
+                 use_bias=True,
+                 logits=True):
+        super(VDCNN, self).__init__()
+        self.num_classes = num_classes
+        self.depth = depth
+        self.seqlen = seqlen
+        self.embed_dim = embed_dim
+        self.shortcut = shortcut
+        self.pool_type = pool_type
+        self.proj_type = proj_type
+        self.use_bias = use_bias
+        self.logits = True
+
+        assert pool_type in ['max', 'k_max', 'conv']
+        assert proj_type in ['conv', 'identity']
+        self.n_blocks = N_BLOCKS[depth]
+
+        self.embed_char = layers.Embedding(seqlen, embed_dim)
+        self.conv = layers.Conv1D(64, 3, strides=1, padding='same', use_bias=use_bias)
+
+        # Convolutional Block 64
+        self.conv_block_64 = []
+        for _ in range(self.n_blocks[0] - 1):
+            self.conv_block_64.append(ConvBlock(64, 3, use_bias, shortcut))
+        self.conv_block_64.append(ConvBlock(64, 3, use_bias, shortcut, pool_type=pool_type, proj_type=proj_type))
+
+        # Convolutional Block 128
+        self.conv_block_128 = []
+        for _ in range(self.n_blocks[1] - 1):
+            self.conv_block_128.append(ConvBlock(128, 3, use_bias, shortcut))
+        self.conv_block_128.append(ConvBlock(128, 3, use_bias, shortcut, pool_type=pool_type, proj_type=proj_type))
+
+        # Convolutional Block 256
+        self.conv_block_256 = []
+        for _ in range(self.n_blocks[2] - 1):
+            self.conv_block_256.append(ConvBlock(256, 3, use_bias, shortcut))
+        self.conv_block_256.append(ConvBlock(256, 3, use_bias, shortcut, pool_type=pool_type, proj_type=proj_type))
+
+        # Convolutional Block 512
+        self.conv_block_512 = []
+        for _ in range(self.n_blocks[3] - 1):
+            self.conv_block_512.append(ConvBlock(512, 3, use_bias, shortcut))
+        self.conv_block_512.append(ConvBlock(512, 3, use_bias, shortcut, pool_type=None, proj_type=None))
+
+        self.k_maxpool = KMaxPooling(k=8)
+        self.flatten = layers.Flatten()
+
+        # Dense layers
+        self.fc1 = layers.Dense(2048, activation='relu')
+        self.fc2 = layers.Dense(2048, activation='relu')
+        self.out = layers.Dense(num_classes)
+
+    def call(self,
+             x):
+        x = self.embed_char(x)
+        print('embed:', x.shape)
+        x = self.conv(x)
+        print('conv:', x.shape)
+
+        for l in self.conv_block_64:
+            x = l(x)
+        print('conv_block_64:', x.shape)
+
+        for l in self.conv_block_128:
+            x = l(x)
+        print('conv_block_128:', x.shape)
+
+        for l in self.conv_block_256:
+            x = l(x)
+        print('conv_block_256:', x.shape)
+
+        for l in self.conv_block_512:
+            x = l(x)
+        print('conv_block_512:', x.shape)
+
+        x = self.k_maxpool(x)
+        print('k_maxpool_8:', x.shape)
+        x = self.flatten(x)
+        print('flatten:', x.shape)
+
+        x = self.fc1(x)
+        x = self.fc2(x)
+        out = self.out(x)
+        print('out:', out.shape)
+
+        if self.logits:
+            return out
+        
+        return tf.nn.softmax(out)
 
 if __name__ == "__main__":
-    model = VDCNN(10, depth=9, shortcut=False, pool_type='max')
+    x = tf.zeros([4, 1024])
+    model = VDCNN(10, depth=9, shortcut=True, pool_type='max', proj_type='identity')
+    out = model(x)
+    model.summary()
+
+    print()
+    model = VDCNN(10, depth=17, shortcut=True, pool_type='max', proj_type='identity')
+    out = model(x)
+    model.summary()
+
+    print()
+    model = VDCNN(10, depth=29, shortcut=True, pool_type='max', proj_type='conv')
+    out = model(x)
+    model.summary()
+
+    print()
+    model = VDCNN(10, depth=49, shortcut=True, pool_type='max', proj_type='conv')
+    out = model(x)
     model.summary()
